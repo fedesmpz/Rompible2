9d7ffe022f63012b0c5ad0deec371bbaa1cbdda7
Juan <juampi.parzybal@gmail.com>
2023-06-08 12:42:59 -0300

    creando un form

---
+++ rompible/src/Form.jsx
@@ -0,0 +1,13 @@
+const Form = () => {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="email">Email:</label>
+                <input type="email" />
+                <button type="submit">Mandar</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
